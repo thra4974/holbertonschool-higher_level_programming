d074d40df45d053388e65dc5dc00f30223ac5c7b
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-28 16:26:14 -0500

    fixed formatting for 6 comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,8 @@
 for n in range(1, 100):
     if n // 10 == n % 10:
         pass
-    elif (n - ((n%10)*(10)) + (n//10))%3 == 0 and (n - ((n%10)*(10)) + (n//10)) > 0:
+    elif (n - ((n % 10) * (10)) + (n // 10)) % 3 == 0
+    and (n - ((n % 10)*(10)) + (n//10)) > 0:
         print("nope")
     else:
         print("{0:0=2d}, ".format((n)), end='')
