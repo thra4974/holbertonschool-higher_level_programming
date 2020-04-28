ba76dd142979a34cc9cd71e45850c102306898cd
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-28 14:36:15 -0500

    fixed range in 3-print_alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z')):
-    if chr(letter) is 'q' or chr(letter) is 'e':
+for letter in range(ord('a'), ord('z') + 1):
+    if chr(letter) == 'q' or chr(letter) == 'e':
         pass
     else:
         print("{}".format(chr(letter)), end='')
