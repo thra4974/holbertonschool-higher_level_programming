1a77b4bbfb691fa367b969fce9b8692562832894
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-28 18:32:00 -0500

    added 8-uppercase.py and 8.main

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+
+    res = ''
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            res += chr(ord(c) - 32)
+        else:
+            res += chr(ord(c))
+    print(res)
