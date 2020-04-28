0b0515fda6fcaa3fb817613a8e9774a28f254264
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-28 13:36:01 -0500

    fixed conditional statements in 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,21 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
-if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format
-          (number, last))
-elif last == 0:
-    print("Last digit of {:d} is {:d} and is 0".format
-          (number, last))
-elif last < 6 and last != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
-          (number, last))
+nLast = (last * -1)
+if (number > 0):
+    if last > 5:
+        print("Last digit of {:d} is {:d} and is greater than 5".format
+              (number, last))
+    elif last == 0:
+        print("Last digit of {:d} is {:d} and is 0".format
+              (number, last))
+    elif last < 6 and last != 0:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
+              (number, last))
+elif (number < 0):
+    if last == 0:
+        print("Last digit of {:d} is {:d} and is 0".format
+              (number, last))
+    elif last < 6 and last != 0:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format
+              (number, nLast))
