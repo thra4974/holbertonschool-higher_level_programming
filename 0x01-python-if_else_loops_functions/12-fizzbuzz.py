59c5adb675cd0d0752f0599207d5d48af25c80e5
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-28 19:25:12 -0500

    fixed fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
+
     for n in range(1, 101):
-        if n % 3 == 0:
-            print("Fizz ", end="")
+        if n % 15 == 0:
+            print("FizzBuzz", end=" ")
         elif n % 5 == 0:
-            print("Buzz ", end="")
-        elif n % 15 == 0:
-            print("FizzBuzz ", end="")
+            print("Buzz", end=" ")
+        elif n % 3 == 0:
+            print("Fizz", end=" ")
         else:
             print("{:d} ".format(n), end="")
