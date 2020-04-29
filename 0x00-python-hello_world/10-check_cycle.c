7249ab47d0fe4269c80f87f960998c3dc75e6492
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-29 00:55:21 -0500

    added linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * check_cycle - checks for a cycle in a linked list
+ * @list: pointer to linked list
+ * Return: 1 if loop found, 0 if no loop
+ */
+
 int check_cycle(listint_t *list)
 {
-	
+	listint_t *DrEggMan = list;
+	listint_t *Sonic = list;
+
+	while (DrEggMan && Sonic && Sonic->next)
+	{
+		DrEggMan = DrEggMan->next;
+		Sonic = Sonic->next->next;
+
+		if (DrEggMan == Sonic)
+			return (1);
+	}
+	return (0);
 }
