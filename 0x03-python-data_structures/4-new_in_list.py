1d47d98037137bc640f5c2ccb0c58e7beb44e8aa
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-05 17:23:56 -0500

    fixed shebang

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 def new_in_list(my_list, idx, element):
-    new = my_list[:]
+    new = my_list.copy()
     if idx < 0:
         return my_list
     if idx >= len(my_list):
