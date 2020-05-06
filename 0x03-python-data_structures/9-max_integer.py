5077cce41576b48286e2e1d780a52feedb48e635
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-05 19:04:06 -0500

    fixed 9-max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,11 @@
 
 
 def max_integer(my_list=[]):
+    maxn = my_list[0]
     if len(my_list) == 0:
         return None
     else:
         for n in range(0, len(my_list)):
-            if my_list[n] > my_list[0]:
-                return (my_list[n])
+            if my_list[n] > maxn:
+                maxn = my_list[n]
+        return maxn
