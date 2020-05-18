a0d90d91c65cab3e94b64af23142d3405ae36aba
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-18 12:55:36 -0500

    removed typeErrror from 1-safe

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,5 @@ def safe_print_integer(value):
                 try:
                         print("{:d}".format(value))
                         return True
-                except TypeError:
+                except:
                         return False
