b3929bcbac2be2f91008c619a22c8ddc3c41ef95
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-18 13:04:38 -0500

    removed for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,13 +3,12 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for n in my_list:
-        try:
-            new_list = my_list[:x]
-            str1 = ''.join(str(n) for n in new_list)
-            print("{}".format(str1))
-            for n in str1:
-                count += 1
-            return (count)
-        except:
-            print("Failed to print list")
+    try:
+        new_list = my_list[:x]
+        str1 = ''.join(str(n) for n in new_list)
+        print("{}".format(str1))
+        for n in str1:
+            count += 1
+        return (count)
+    except:
+        print("Failed to print list")
