6166fea297b1aa6f4f6d1b695f99a3e91aae56b2
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-18 15:53:14 -0500

    fixed ordering of exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,9 +7,9 @@ def list_division(my_list_1, my_list_2, list_length):
                 res = 0
                 try:
                         res = my_list_1[n] / my_list_2[n]
-                except TypeError:
-                        print("division by 0")
                 except ZeroDivisionError:
+                        print("division by 0")
+                except TypeError:
                         print("wrong type")
                 except IndexError:
                         print("out of range")
