4ec4c2dcb0628b6a1935f20805450cfc79c7d622
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-18 12:53:07 -0500

    Added 1-safe_print_integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+        if isinstance(value, int):
+                try:
+                        print("{:d}".format(value))
+                        return True
+                except TypeError:
+                        return False
