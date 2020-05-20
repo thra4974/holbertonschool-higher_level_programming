1549093c393dba57cd0daa2c1a49c256c63d9ebb
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-19 19:24:52 -0500

    added comment to top of each file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""defines class Square"""
 
 
 class Square:
+    """
+    square with private instance atr size
+    """
     def __init__(self, size=0):
+        """
+        initialize square
+
+        Args:
+            size: size of width/height of square
+        """
         self.__size = size
 
     @property
     def size(self):
+        """
+        checks type of size and value
+        """
         return self.__size
 
     @size.setter
@@ -20,4 +33,7 @@ class Square:
             raise TypeError("size must be an integer")
 
     def area(self):
+        """
+        get area of square
+        """
         return self.__size**2
