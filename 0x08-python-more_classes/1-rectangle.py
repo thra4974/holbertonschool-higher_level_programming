2af7d4fee21ee16195c8194cf82d7690553ebd7d
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-26 11:47:30 -0500

    fixed conditional statements in 1-rec

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         set height
         """
         if isinstance(value, int):
-            if value < 0:
+            if value <= 0:
                 raise ValueError("height must be >= 0")
             else:
                 self.__height = value
@@ -48,7 +48,7 @@ class Rectangle:
         set width
         """
         if isinstance(value, int):
-            if value < 0:
+            if value <= 0:
                 raise ValueError("width must be >= 0")
             else:
                 self.__width = value
