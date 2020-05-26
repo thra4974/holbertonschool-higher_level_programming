2ed6825c1a96026d1085092423cd849504323e0e
thra4974 <tristan.thrasher-1@ou.edu>
2020-05-26 13:15:23 -0500

    added 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -112,7 +112,15 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """
+        return rect as a square
+        """
+        new_rect = cls(size, size)
+        return new_rect
