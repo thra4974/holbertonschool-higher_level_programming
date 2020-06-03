8d05ce03aca83fadbd70aa01b43ddc402bb0aa62
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-03 18:13:48 -0500

    fixed return statement in 4-inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,5 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ returns True if object is a subclass of a_class"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
