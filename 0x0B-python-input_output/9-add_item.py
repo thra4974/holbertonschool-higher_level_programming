0734d45aab0fc1d991bd49e126fbb26102e2f132
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-03 17:06:24 -0500

    added whitespace around = operators

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,9 +7,9 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 f = "add_item.json"
 try:
-    my_list= load_from_json_file(f)
+    my_list = load_from_json_file(f)
 except:
-    my_list=[]
+    my_list = []
 for arg in argv[1:]:
     my_list.append(arg)
 save_to_json_file(my_list, f)
