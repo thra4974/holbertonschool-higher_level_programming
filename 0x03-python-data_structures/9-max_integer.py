c777fb908143c6ba4929452a10a6242e0995eb8f
thra4974 <tristan.thrasher-1@ou.edu>
2020-06-24 12:08:14 -0500

    fixed none in 9-max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,12 @@
 
 
 def max_integer(my_list=[]):
-    maxn = my_list[0]
-    if len(my_list) == 0:
-        return None
-    else:
-        for n in range(0, len(my_list)):
-            if my_list[n] > maxn:
-                maxn = my_list[n]
-        return maxn
+    if my_list:
+        maxn = my_list[0]
+        if len(my_list) == 0:
+            return None
+        else:
+            for n in range(0, len(my_list)):
+                if my_list[n] > maxn:
+                    maxn = my_list[n]
+            return maxn
